0c5e7c52596192886254c6e09ed20d32a646fcef
Malith Ranasinghe <ranasinghemalith20@gmail.com>
2025-12-02 17:06:37 +0530

    chakra ui

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,27 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+// eslint.config.js
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettier from 'eslint-plugin-prettier'
+import configPrettier from 'eslint-config-prettier'
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
 
-export default eslintConfig;
+  // Disable rules that conflict with Prettier
+  configPrettier,
+
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error', // Show Prettier issues as ESLint errors
+    },
+  },
+
+  // Override default ignores
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+])
